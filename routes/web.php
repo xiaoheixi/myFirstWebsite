ea9979d1cb111f4d1a1ec2cdfb3cc5e4fccd1265
xiaoheixi <justinzhao893@gmail.com>
2020-08-31 18:37:07 +1000

    boom!

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,71 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/administrator', function () {
+    return view('administrator');
 });
 
 Route::get('/adminLogin', function () {
     return view('adminLogin');
 });
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/editContactDetails', function () {
+    return view('editContactDetails');
+});
+
+Route::get('/editHome', function () {
+    return view('editHome');
+});
+
+Route::get('/editNavigationBar', function () {
+    return view('editNavigationBar');
+});
+
+Route::get('/editNews', function () {
+    return view('editNews');
+});
+
+Route::get('/editServices', function () {
+    return view('editServices');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/orders', function () {
+    return view('orders');
+});
+
+Route::get('/payment', function () {
+    return view('payment');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/thanks', function () {
+    return view('thanks');
+});
+
