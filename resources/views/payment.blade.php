5bb120ad6ceb09340a7e6e786abdcccc1d10b65a
xiaoheixi <justinzhao893@gmail.com>
2020-09-06 14:07:16 +1000

    Payment Form added to payment.blade.php.

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -41,6 +41,15 @@
   <label for="square">Square</label><br>
                             <div class="col-md-12"><a href="thanks.html" class="btn btn-success" style="width: 100%;" role="button">Checkout </a></div>
                         </form>
+                        <form class="w3-container w3-display-middle w3-card-4 " method="POST" id="payment-form"  action="/payment/add-funds/paypal">
+  {{ csrf_field() }}
+  <h2 class="w3-text-blue">Payment Form</h2>
+  <p>Demo PayPal form - Integrating paypal in laravel</p>
+  <p>      
+  <label class="w3-text-blue"><b>Enter Amount</b></label>
+  <input class="w3-input w3-border" name="amount" type="text"></p>      
+  <button class="w3-btn w3-blue">Pay with PayPal</button></p>
+</form>
                     </div>
                 </div>
             </div>
