d045fc4cbb53e99d5fd17b3abeac6d95af96ec49
xiaoheixi <justinzhao893@gmail.com>
2020-09-07 19:19:39 +1000

    checkout.blade.php form is updated so that PayPal works.

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -69,6 +69,23 @@
             <div class="col-lg-5 col-12">
                 <div class="row mx-0">
                     <div class="col-12 card bg-light mx-auto">
+                    <div class="w3-container">
+                    @if ($message = Session::get('success'))
+                    <div class="w3-panel w3-green w3-display-container">
+                        <span onclick="this.parentElement.style.display='none'"
+                            class="w3-button w3-green w3-large w3-display-topright">&times;</span>
+                        <p>{{ $$message }}</p>
+                    </div>
+                    <?php Session::forget('success');?>
+                    @endif
+                    @if ($message = Session::get('error'))
+                    <div class="w3-panel w3-red w3-display-container">
+                        <span onclick="this.parentElement.style.display='none'"
+                            class="w3-button w3-red w3-large w3-display-topright">&times;</span>
+                            <p>{{ $$message }}</p>
+                            </div>
+                    <?php Session::forget('error');?>
+                    @endif
                     <form class="w3-container w3-display-middle w3-card-4 " method="POST" id="payment-form"  action="{!! URL::to('paypal') !!">
   <h2 class="w3-text-blue">Payment Form</h2>
   <p>
@@ -88,6 +105,7 @@
   <input class="w3-input w3-border" name="amount" type="text"></p>      
   <button class="w3-btn w3-blue">Pay with PayPal</button></p>
 </form>
+</div>
                     </div>
                 </div>
             </div>
