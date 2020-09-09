af35c65f330a1e699638d47c8fef1ef9cc4be0bc
xiaoheixi <justinzhao893@gmail.com>
2020-09-09 21:24:05 +1000

    Tried

--- resources/views/page/create.blade.php
+++ resources/views/page/create.blade.php
@@ -4,9 +4,27 @@
 <div class="row">
     <div class="col-md-12">
         <br />
-        <h3 aling="center">Add Data</h3>
+        <h3 aling="center">Add Page</h3>
         <br />
+        @if(count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+            <!--Prints valdiation errors on this page!-->
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+            </ul>
+        </div>
+        @endif
+        <!--Checks if success has been returend from PageController.php!-->
+        @if(\Session::has('success'))
+        <div class="alert alert-success">
+            <p>{{ \Session::get('success') }}</p>
+        </div>
+        @endif
+
         <form method="post" action="{{url('Pages')}}">
+            {{csrf_field()}}
             <div class="form-group">
                 <input type="text" name="title" class="form-control" placeholder="Enter page title" />
             </div>
