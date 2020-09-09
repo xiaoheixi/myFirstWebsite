efc147cb1f47c19b31068664abee37fc277e1a41
xiaoheixi <justinzhao893@gmail.com>
2020-09-09 20:08:18 +1000

    Updated create.blade.php!

--- resources/views/page/create.blade.php
+++ resources/views/page/create.blade.php
@@ -1,1 +1,15 @@
 @extends('pageManagement')
+<!-- Content being displayed!-->
+@section('pageManagement')
+<div class="row">
+    <div class="col-md-12">
+        <br />
+        <h3 aling="center">Add Data</h3>
+        <br />
+        <form method="post" action="{{url('Pages')}}">
+
+        </form>
+    </div>
+</div>
+<!-- Closes the directive!-->
+@endsection
