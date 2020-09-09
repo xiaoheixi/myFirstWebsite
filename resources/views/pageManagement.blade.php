68caa308f3b4996690c9ab04b781dc89eadbebb5
xiaoheixi <justinzhao893@gmail.com>
2020-09-09 19:55:08 +1000

    Created create.blade.php inside page folder in views folder.

--- resources/views/pageManagement.blade.php
+++ resources/views/pageManagement.blade.php
@@ -7,7 +7,9 @@
     <title>Page Management</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
-    <body>
-
-    </body>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
 </html>
