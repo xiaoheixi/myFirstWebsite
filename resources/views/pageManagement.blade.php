e43777967a622f7bf0af614f383750988bd65274
xiaoheixi <justinzhao893@gmail.com>
2020-09-09 19:51:11 +1000

    pageManagement.blade.php updated.

--- resources/views/pageManagement.blade.php
+++ resources/views/pageManagement.blade.php
@@ -1,6 +1,13 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page Management</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
     <body>
-        
+
     </body>
 </html>
