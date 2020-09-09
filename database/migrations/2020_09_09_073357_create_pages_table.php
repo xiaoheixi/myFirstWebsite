6bac2d50d0c65a195f6d69b4b4665b52bea4491b
xiaoheixi <justinzhao893@gmail.com>
2020-09-09 17:37:40 +1000

    Page table is created.

--- database/migrations/2020_09_09_073357_create_pages_table.php
+++ database/migrations/2020_09_09_073357_create_pages_table.php
@@ -15,6 +15,12 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('Title');
+            $table->string('Link');
+            $table->int('Text Size');
+            $table->string('Text Colour');
+            $table->string('Background Colour');
+            $table->string('Font');
             $table->timestamps();
         });
     }
