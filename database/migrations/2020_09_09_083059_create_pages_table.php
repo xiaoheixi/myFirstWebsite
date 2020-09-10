49cd422f7bd6d2a8d7ad41376745d77ec2c1bd00
xiaoheixi <justinzhao893@gmail.com>
2020-09-10 18:55:33 +1000

    Updated pages migration.

--- database/migrations/2020_09_09_083059_create_pages_table.php
+++ database/migrations/2020_09_09_083059_create_pages_table.php
@@ -16,11 +16,7 @@ class CreatePagesTable extends Migration
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('link');
-            $table->integer('textSize');
-            $table->string('textColour');
-            $table->string('backgroundColour');
-            $table->string('font');
+            $table->string('URI');
             $table->timestamps();
         });
     }
