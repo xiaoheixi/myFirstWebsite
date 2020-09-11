8afa5fff835c0d4366a4f29bccb81155766ce458
xiaoheixi <justinzhao893@gmail.com>
2020-09-11 13:54:08 +1000

    q

--- resources/views/page/dynamicPage.blade.php
+++ resources/views/page/dynamicPage.blade.php
@@ -1,5 +1,12 @@
-@extends('pageManagement')
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
 
-@section('content')
-<title>$title</title>
-@endsection
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
